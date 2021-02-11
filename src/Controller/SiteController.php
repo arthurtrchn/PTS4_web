753e7ec1733be87d2c779ac75315d17fb6de4653
Thibault <thibaultderouin5@gmail.com>
2021-02-11 17:50:17 +0100

    Initial commit

---
+++ src/Controller/SiteController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SiteController extends AbstractController
+{
+    /**
+     * @Route("/", name="accueil")
+     */
+    public function index(): Response
+    {
+        return $this->render('site/index.html.twig', [
+            'controller_name' => 'SiteController',
+        ]);
+    }
+
+    /**
+     * @Route("/devis", name="devis")
+     */
+    public function devis(): Response
+    {
+        return $this->render('site/devis.html.twig', [
+            'controller_name' => 'SiteController',
+        ]);
+    }
+}
